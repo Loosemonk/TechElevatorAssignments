590e1b1b92958834c55fd2ce3965c0867cdcad1a
Benjamin Hines <Loosemonk@gmail.com>
2020-06-25 11:56:49 -0400

    M3 Day3

--- module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-3/03-ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -11,10 +11,33 @@ import java.util.List;
 @RequestMapping("/auctions")
 public class AuctionController {
 
-    private AuctionDAO dao;
+	private AuctionDAO dao;
 
-    public AuctionController() {
-        this.dao = new MemoryAuctionDAO();
-    }
+	public AuctionController() {
+		this.dao = new MemoryAuctionDAO();
+	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Auction> list(@RequestParam(required = false, defaultValue = "") String title_like,
+			@RequestParam(required = false, defaultValue = "0.0") double currentBid_lte) {
+		String empty = "";
+		if (!title_like.equals(empty)) {
+			return dao.searchByTitle(title_like);
+		}
+		if (currentBid_lte > 0.0) {
+			return dao.searchByPrice(currentBid_lte);
+		}
+		return dao.list();
+	}
+
+	@RequestMapping(path = "{id}", method = RequestMethod.GET)
+	public Auction get(@PathVariable int id) {
+		return dao.get(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Auction addAuction(@RequestBody Auction auction) {
+		return dao.create(auction);
+
+	}
 }
