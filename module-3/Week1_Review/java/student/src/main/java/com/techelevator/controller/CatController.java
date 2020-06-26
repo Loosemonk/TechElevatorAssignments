b70fe39a68949d636fc20d9e57f98971a2e2d739
Benjamin Hines <Loosemonk@gmail.com>
2020-06-26 16:37:26 -0400

    M3 Week 1 Review

--- module-3/Week1_Review/java/student/src/main/java/com/techelevator/controller/CatController.java
+++ module-3/Week1_Review/java/student/src/main/java/com/techelevator/controller/CatController.java
@@ -7,11 +7,13 @@ import com.techelevator.model.CatPic;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-
+@RestController
+@RequestMapping("/api/cards")
 public class CatController {
 
     private CatCardDAO catCardDao;
@@ -23,6 +25,41 @@ public class CatController {
         this.catFactService = catFactService;
         this.catPicService = catPicService;
     }
+@RequestMapping(path = "/random", method = RequestMethod.GET)
+public CatCard getRandomCatCard() {
+	CatCard newCard = new CatCard();
+    String catFact = catFactService.getFact().getText();
+	newCard.setCatFact(catFact);
+	String imgUrl = catPicService.getPic().getFile();
+	newCard.setImgUrl(imgUrl);
+	return newCard;
+}
 
+ //POST /api/cards
+@ResponseStatus(HttpStatus.CREATED)
+@RequestMapping(path = "", method = RequestMethod.POST)
+public void saveNewCard(@Valid @RequestBody CatCard card) {
+	catCardDao.save(card);
+}
 
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@RequestMapping(path="/{id}", method = RequestMethod.PUT)
+public void updateCard(@Valid @RequestBody CatCard card, @PathVariable long id) {
+	catCardDao.update(id, card);
+}
+//GET /api/cards
+@RequestMapping(path = "", method = RequestMethod.GET)
+public List<CatCard> getAllCards(){
+	return catCardDao.list();
+}
+@RequestMapping(path = "/{id}", method = RequestMethod.GET)
+public CatCard getCard(@PathVariable long id){
+	return catCardDao.get(id);
+}
+@ResponseStatus(HttpStatus.NO_CONTENT)
+@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+public void  deleteCard(@PathVariable long id) {
+	 catCardDao.delete(id);
+
+}
 }
