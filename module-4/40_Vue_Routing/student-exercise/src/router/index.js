a91af44491a2ed70956942e35a965478d9b03532
Benjamin Hines <Loosemonk@gmail.com>
2020-07-27 16:59:00 -0400

    Vue Routing

--- module-4/40_Vue_Routing/student-exercise/src/router/index.js
+++ module-4/40_Vue_Routing/student-exercise/src/router/index.js
@@ -1,9 +1,38 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '@/views/Home';
+import MyBooks from '@/views/MyBooks';
+import NewBook from '@/views/NewBook';
+import DetailsView from '@/views/DetailsView';
+
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    name: 'home', //recommended but not required
+    component: Home
+  },
+  {
+    path: '/myBooks',
+    name: 'myBooks',
+    component: MyBooks
+  },
+  {
+    path: '/addBook',
+    name: 'newBook',
+    component: NewBook
+  },
+  {
+    path: '/book/:isbn',
+    name: 'Book',
+    component: DetailsView
+  }
+
+
+ 
+
 
 ];
 
